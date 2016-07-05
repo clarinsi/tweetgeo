45bab2ddc1b8de1de97a7ed54f1df18f3090d00d
Nikola Ljubešić <nljubesi@gmail.com>
2016-07-05 16:17:43 +0200

    Yet another dataset update

--- 3-analysis/analyse.R
+++ 3-analysis/analyse.R
@@ -1,9 +1,4 @@
-#dataset="../../hbs-twitter-space/hrsrTweets.var.gz"
-#variables=c("tid","user","time","lat","lon","text","yat","da_je_li","sta_sto","bre","mnogo","drug","rdrop","c_ch","months")
-#df=read.csv(gzfile(dataset),header=T,quote="",sep="\t")
-#df=subset(df,select=variables)
-#write.table(df,file=gzfile("tweets.gz","w"),sep="\t",col.names=T,quote=F,row.names=F)
-dataset="tweets.toy.gz"
+dataset="../tweets.gz"
 df=read.csv(gzfile(dataset),header=T,quote="",sep="\t")
 variables=c("yat","da_je_li","sta_sto","bre","mnogo","drug","rdrop","c_ch","months")
 #for (v in variables){ print(v);print(summary(df$v))}
@@ -11,4 +6,4 @@ print(summary(df))
 tusers=table(df$user)
 print("Number of users: ")
 print(length(tusers))
-print(length(tusers[tusers>=100])
+print(length(tusers[tusers>=100]))
