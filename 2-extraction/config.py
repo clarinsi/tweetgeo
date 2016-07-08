2a98dbe9bbf77020b6258585c2cddabec58fc286
Nikola Ljubešić <nljubesi@gmail.com>
2016-07-08 07:15:56 +0000

    Representative configs

--- 2-extraction/config.py
+++ 2-extraction/config.py
@@ -4,10 +4,9 @@ from extraction import *
 # languages used by user_langid in filter_by_user.py
 LANG=['hr','sr','bs']
 
-#DB='../1-collection/IberiaDSL.db'
-DB='/home/nikola/papers/2016_yutter/yutter.db'
+DB='../1-collection/hbs-twitter.db'
 
-TSV='yutter.tsv'
+TSV='hbs-twitter.tsv'
 
 import os
 RESOURCES={}
@@ -35,4 +34,4 @@ def tokenize(text):
 
 EXTRACTION_STATUS=[langf,]
 EXTRACTION_TEXT=[(lexicon_choice,(RESOURCES['stosta'],)),(regex_choice,({'dali':re.compile(r'\b(da li)\b',re.UNICODE),'jeli':re.compile(r'\b(je li)\b',re.UNICODE)},))]
-EXTRACTION_NORMALISED=[(lexicon_choice,(RESOURCES['yat'],)),(lexicon_choice,(RESOURCES['prijateljdrug'],)),(lexicon_choice,(RESOURCES['rdrop'],)),(lexicon_choice,(RESOURCES['breba'],)),(lexicon_choice,(RESOURCES['months'],)),(lexicon_choice,(RESOURCES['mnogo'],))]
+EXTRACTION_NORMALISED=[(lexicon_choice,(RESOURCES['yat'],)),(lexicon_choice,(RESOURCES['prijateljdrug'],)),(lexicon_choice,(RESOURCES['rdrop'],)),(lexicon_choice,(RESOURCES['breba'],)),(lexicon_choice,(RESOURCES['months'],)),(lexicon_choice,(RESOURCES['mnogo'],)),(regex_choice,({'noise':re.compile(r'^i\'m at '),'noise':re.compile(ur'по курсу')},))]
